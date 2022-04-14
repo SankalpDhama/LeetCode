745ca18c9d952d7cf0a216a8c9b4805c1fc8d406
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-04-14 14:45:36 +0530

    Time: 1 ms (98.81%), Space: 40 MB (81.55%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,7 +1,38 @@
 import java.lang.Math;
 class Solution {
+    //finding the squareRoot by Binary Search
+    static int binarySearchSqrt(int target){
+        if(target==0 || target==1){
+            return target;
+        }
+        long start=0;
+        long end=target;
+        long mid=0;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            //System.out.println(mid);
+            if((mid-1)*(mid-1)<target && (mid*mid)>target){
+                //System.out.println("hello bitch");
+                
+                return (int)mid-1;
+            }
+            else if(mid*mid<target){
+                //System.out.println(mid*mid+"target is greater");
+                //System.out.println(start+"this is start");
+               start=mid+1;
+            }else if(mid*mid>target){
+               // System.out.println(mid*mid+"target is less");
+                //System.out.println(end+"end");
+                
+                end=mid-1;                 
+            }
+            else{
+                return (int)mid;
+            }
+        }
+        return -1;
+    }
     public int mySqrt(int x) {
-        int a=(int)Math.sqrt(x);
-        return a;
+       return binarySearchSqrt(x);
     }
 }
