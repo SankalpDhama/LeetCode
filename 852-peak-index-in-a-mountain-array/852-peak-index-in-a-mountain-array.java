73aa305883fbf9fbd52fa38874abe1861df9c128
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-04-14 23:16:46 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (81.44%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -5,14 +5,16 @@ class Solution {
         int mid=0;
         while(start<=end){
             mid=start+(end-start)/2;            
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                return mid;
-            }else if(arr[mid]<arr[mid+1] && arr[mid]>arr[mid-1]){
+            // if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+            //     return mid;
+        //    }
+            if(arr[mid]<arr[mid+1] && arr[mid]>arr[mid-1]){
                 start=mid+1;
             }else if(arr[mid]>arr[mid+1] && arr[mid]<arr[mid-1]){
                 end=end-1;
-            }else{
-                return -1;
+            }
+            else{
+                return mid;
             }
         }
         return -1;
