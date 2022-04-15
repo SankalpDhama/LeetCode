cadf2dc296ae977182a84ff21eec1ba5f8766670
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-04-15 09:47:16 +0530

    Time: 0 ms (100.00%), Space: 40 MB (81.72%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp=new ListNode();
+        temp=head;
+        ListNode answerNode=head;        
+        int countLength=1;
+        while(temp.next!=null){
+            temp=temp.next;
+            countLength++;
+        }
+        // //System.out.println(countLength);
+        // if(countLength%2==0){
+        //     countLength=countLength/2;
+        //    System.out.println(countLength+"this is even length");
+        // }else{
+        //     countLength=countLength/2;
+        //    // System.out.println(countLength+"this is odd length");
+        // }
+        countLength=countLength/2;
+        
+        for(int i=1;i<=countLength;i++){
+            answerNode=answerNode.next;
+            //System.out.println()
+        }
+        return answerNode;
+    }
+}
