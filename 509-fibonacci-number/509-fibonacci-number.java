f0f5a7a63be04fb9908348acc89f13119de8b60d
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-05-25 19:48:40 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (89.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,13 @@
 class Solution {
-    int fibo(int n){
-        if(n==1 ){
-            return 1;
-        }
-        else if(n==0){
-            return 0;
-        }
-        else{
-            return fibo(n-1)+fibo(n-2);
-        }
-    }
     public int fib(int n) {
-      return fibo(n);  
+        int a=0;
+        int b=1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            a=b;
+            b=c;
+            c=a+b;
+        }
+        return c;
     }
 }
