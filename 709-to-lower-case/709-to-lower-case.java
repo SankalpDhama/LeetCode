f300a584e7e6632f684d8595fba7088336786d31
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-06-02 23:40:48 +0530

    Time: 1 ms (63.88%), Space: 42.1 MB (53.39%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] lowerString=new char[s.length()];
+        for(int i=0;i<s.length();i++){
+            if(Character.isUpperCase(s.charAt(i))){
+                int a=(int)s.charAt(i);
+                a=a+32;
+                lowerString[i]=(char)a;
+            }
+            else{
+                lowerString[i]=s.charAt(i);
+            }
+        }
+        return new String(lowerString);
+    }
+}
