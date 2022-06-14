85cb787e43be8ae4a245bb8c2a0b5afc7d9da03e
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-06-14 17:47:15 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (39.35%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        boolean isNine=false;
-        for(int i=0;i<digits.length;i++){
-            if(digits[i]==9){
-                isNine=true;
+        int digitsSize=digits.length;
+        int i=0;
+        for(i=digitsSize-1;i>=0;i--){
+            digits[i]=digits[i]+1;
+            if(digits[i]==10){
+                digits[i]=0;
             }else{
-                isNine=false;
                 break;
             }
+        }if(i==-1){
+            int[] digitsPlusOne=new int[digitsSize+1];
+            digitsPlusOne[0]=1;
+            return digitsPlusOne;
         }
-        int[] plusOne=new int[digits.length+1];
-        int i=digits.length-1;
-        try{
-            while(digits[i]==9){
-            digits[i]=0;
-            plusOne[i+1]=0;
-            i--;
-      }
-        digits[i]=digits[i]+1;
-           }catch(Exception e){
-            
-        };
-           plusOne[0]=1;       
-        if(isNine){
-            return plusOne;
-        }else{
-            return digits;
-        }
- }
+        return digits;
+    }
 }
