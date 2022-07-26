373248ca363d6541090318bc5abdb552f398ed77
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-07-27 00:14:39 +0530

    Time: 1 ms (99.84%), Space: 44.2 MB (36.19%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,33 +1,21 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int size1=nums1.length;
-        int size2=nums2.length;
-        HashMap<Integer,Integer> intersect=new HashMap<>();
-        ArrayList<Integer> answerList=new ArrayList<>(); 
-        int count=1;
-        for(int i=0;i<size1;i++){
-            if(intersect.containsKey(nums1[i])){
-               count=intersect.get(nums1[i]);
-               intersect.put(nums1[i],++count);
-               count=1; 
-            }else{
-                intersect.put(nums1[i],count);
+        int[] arr=new int[1001];
+        List<Integer> answerList=new ArrayList<>();        
+        for(int i=0;i<nums1.length;i++){
+            arr[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(arr[nums2[i]]!=0){
+               answerList.add(nums2[i]);
+               arr[nums2[i]]--; 
             }
         }
-        for(int i=0;i<size2;i++){
-            if(intersect.containsKey(nums2[i])){
-                count=intersect.get(nums2[i]);
-                if(count>0){
-                   answerList.add(nums2[i]); 
-                }
-                intersect.put(nums2[i],--count);
-            }
+        int size=answerList.size();
+        int[] ans=new int[size];
+        for(int i=0;i<size;i++){
+            ans[i]=answerList.get(i);
         }
-        int sizeList=answerList.size();
-        int[] answer=new int[sizeList];
-        for(int i=0;i<sizeList;i++){
-            answer[i]=answerList.get(i);
-        }
-        return answer;
+        return ans;
     }
 }
