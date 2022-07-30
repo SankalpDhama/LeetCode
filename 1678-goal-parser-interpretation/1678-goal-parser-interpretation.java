51270c7ef397c13a096e1d74d4ff88bc01c0bf7b
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-07-30 11:55:37 +0530

    Time: 1 ms (90.63%), Space: 42.9 MB (5.83%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,8 +1,16 @@
 class Solution {
     public String interpret(String command) {
-        command=command.replace("()","o");
-        command=command.replace("G","G");
-        command=command.replace("(al)","al");  
-        return command;
+        int size=command.length();
+        StringBuilder soccer=new StringBuilder();
+        for(int i=0;i<size;i++){
+            if(command.charAt(i)=='G'){
+                soccer.append("G");
+            }else if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                soccer.append("al");
+            }else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                soccer.append("o");
+            }
+        }
+        return soccer.toString();
     }
 }
