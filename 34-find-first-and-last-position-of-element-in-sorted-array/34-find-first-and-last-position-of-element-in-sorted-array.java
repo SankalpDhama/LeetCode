e8ceba5fb9bb67afcba5c7848b8f6f486ddeb00d
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-08-02 00:37:51 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (51.26%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,50 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int i=0;
-        int j=nums.length-1;
-        while(i<=j){
-            if(nums[i]!=target){
-                System.out.println(i);
-                i++;
-            }else if(nums[j]!=target){
-                System.out.println(j);
-                j--;
-            }else{
-                return new int[]{i,j};
+    int binaryLeft(int nums[],int target){
+        if(nums[0]==target){
+            return 0;
+        }
+        int start=1;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target && nums[mid-1]!=target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else if(nums[mid]==target && nums[mid-1]==target){
+                end=mid-1;
             }
         }
-        return new int[]{-1,-1};
+        return -1;
+    }
+    int binaryRight(int nums[],int target){
+        if(nums[nums.length-1]==target){
+            return (nums.length-1);
+        }
+        int start=0;
+        int end=nums.length-2;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target && nums[mid+1]!=target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else if(nums[mid]==target && nums[mid+1]==target){
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        if(nums.length==0){
+            return new int[] {-1,-1};
+        }
+        // int left=binaryLeft(nums,target);
+        // int right=binaryRight(nums,target);
+        return new int[] {binaryLeft(nums,target),binaryRight(nums,target)};
     }
 }
