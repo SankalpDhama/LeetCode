8c1cd6b57de1fd9b8a229402db804b132823e424
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-08-20 09:59:00 +0530

    Time: 0 ms (100.00%), Space: 64.5 MB (48.10%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,38 +1,19 @@
 class Solution {
+    public static void reverse(int[] nums,int start,int end){
+        int swap=0;
+        while(start<end){
+            swap=nums[start];
+            nums[start]=nums[end];
+            nums[end]=swap;
+            start++;
+            end--;
+        }
+    }
     public void rotate(int[] nums, int k) {
         int size=nums.length;
-        int rotatingFactor=size;
-        if(size<=1 || size==k){
-            return;
-        }
-        int[] rotatedArray=new int[size];
-        if(size<k){
-            int factor=k/size;
-            factor=factor*size;
-            factor=k-factor;
-            for(int i=0;i<size;i++){
-               if(i+factor<=size-1){
-                rotatedArray[i+factor]=nums[i];
-            }else if(i+factor>size-1){
-               // System.out.println(i);
-                rotatedArray[Math.abs(size-(i+factor))]=nums[i];
-            }
-        } 
-            }
-        else{
-        for(int i=0;i<size;i++){
-            if(i+k<=size-1){
-                rotatedArray[i+k]=nums[i];
-            }else if(i+k>size-1){
-               // System.out.println(i);
-                rotatedArray[Math.abs(size-(i+k))]=nums[i];
-            }
-        }
-        }
-        //System.out.println(Arrays.toString(rotatedArray));
-        for(int i=0;i<size;i++){
-            nums[i]=rotatedArray[i];
-        }
-        
+        k=k%size;
+        reverse(nums,0,size-1-k);
+        reverse(nums,size-k,size-1);
+        reverse(nums,0,size-1);
     }
 }
