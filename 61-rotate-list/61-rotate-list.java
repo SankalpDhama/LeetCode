c57d2133d378ea4f2d26d86fd9faded938bf527f
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-09-03 13:20:29 +0530

    Time: 11 ms (5.56%), Space: 43.7 MB (8.33%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -8,34 +8,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    int listLength(ListNode head){
-        int length=0;
-        ListNode cur=head;
-        while(cur!=null){
-            cur=cur.next;
-            length++;
-        }
-        return length;
-    }
-    ListNode last(ListNode head){
-        ListNode cur=head;
-        while(cur.next!=null){
-            cur=cur.next;
-        }
-        return cur;
-    }
+class Solution {    
     public ListNode rotateRight(ListNode head, int k) {
         if(head==null || head.next==null){
             return head;
         }
-        int length=listLength(head);
+        int length=1;
+        ListNode cur=head;
+        while(cur.next!=null){
+            cur=cur.next;
+            length++;
+            
+        }
         k=k%length;
         if(k==0){
             return head;
         }
-        System.out.println(k+"k");
-       System.out.println("length"+length);
+       // System.out.println(k+"k");
+        //System.out.println("length"+length);
         int index=Math.abs(k-length);
         System.out.println(index+"index");
         int nodeInd=1;
@@ -44,9 +34,9 @@ class Solution {
             newLastNode=newLastNode.next;
             nodeInd++;
         }
-        System.out.println(newLastNode.val+"bheesh");
+        //System.out.println(newLastNode.val+"bheesh");
         ListNode newHead=newLastNode.next;
-        ListNode last=last(head);
+        ListNode last=cur;
         last.next=head;
         newLastNode.next=null;
         return newHead;
