b0256226fbf1a8764c549c0ab1535d71f2329488
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2022-09-06 23:47:05 +0530

    Time: 3 ms (88.23%), Space: 58.6 MB (86.82%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -9,19 +9,7 @@
  * }
  */
 class Solution {
-    ListNode one(ListNode head){
-        ListNode temp=head;
-        ListNode end=head.next;        
-        ListNode prevEnd=head;        
-        while(end.next!=null){
-            prevEnd=prevEnd.next;
-            end=end.next;
-        }
-        end.next=head.next;
-        prevEnd.next=temp;
-        head.next=null;  
-        return end;
-    }
+
     int length(ListNode head){
         ListNode cur=head;
         int len=1;
@@ -36,41 +24,17 @@ class Solution {
         if(head==null || head.next==null){
             return head;
         }
-        if(len==2){
-           // System.out.println("san");
-            ListNode temp=head.next;
-            head.next=null;
-            temp.next=head;
-            return temp;
-        }
-        if(k==1 || k==len){
-            System.out.println("bheehs"+len);
-            return one(head);
-        }
-       // System.out.println(len+"bheesh");
-        if(len==2){
-            ListNode start=head;
-            ListNode end=head.next;
-            start.next=null;
-            end.next=start;
-            head=end;
-            return head;
-        }
         ListNode curStart=head;
         ListNode curEnd=head;
-        for(int i=1;i<k-1;i++){
+        for(int i=1;i<k;i++){
             curStart=curStart.next;
         }
-        for(int i=1;i<len-k;i++){
+        for(int i=1;i<len-(k-1);i++){
             curEnd=curEnd.next;
         }
-        ListNode Start=curStart.next;
-        ListNode End=curEnd.next;
-        curStart.next=End;
-        curEnd.next=Start;
-        ListNode startNext=Start.next;
-        Start.next=End.next;
-        End.next=startNext;
+        int temp=curStart.val;
+        curStart.val=curEnd.val;
+        curEnd.val=temp;
         return head;
     }
 }
