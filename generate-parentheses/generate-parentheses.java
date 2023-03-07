aeb329265d9fe8bf1a29a8ebdd4d9307d36043ac
Sankalp Dhama <87567330+SankalpDhama@users.noreply.github.com>
2023-03-07 16:18:34 +0530

    Time: 2 ms, Memory: 43.4 MB - LeetHub

---
+++ generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution{
+    void backtrack(String bracs,List<String> answer,int open,int close,int n){
+        int size=bracs.length();
+        if(size==2*n){
+            answer.add(bracs);
+            //bracs="";
+            return;
+        }
+            if(open<n){
+                //System.out.println("open brackets "+open);
+                backtrack(bracs+"(",answer,open+1,close,n);                 
+            }
+            if(close<open){
+                //System.out.println("close brackets "+close);
+                backtrack(bracs+")",answer,open,close+1,n);
+            }
+               // System.out.println("open val "+open);        
+        }
+    
+    public List<String> generateParenthesis(int n) {
+        List<String> answer=new ArrayList<>();
+        String bracs="(";
+        backtrack(bracs,answer,1,0,n);
+        return answer;
+    }
+}
